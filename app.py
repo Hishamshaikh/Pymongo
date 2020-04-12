9e49e08ea6cfe354f5c0890b1d5c673101e149b8
Hishamshaikh <63351862+Hishamshaikh@users.noreply.github.com>
2020-04-12 13:52:05 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,9 @@
+import pymongo
+
+uri = "mongodb://127.0.0.1:27017"
+client = pymongo.MongoClient(uri)
+database = client['fullstack']
+collection = database['students']
+
+students =[student['mark'] for student in collection.find({}) if student['mark'] == 99.0]
+print(students)
